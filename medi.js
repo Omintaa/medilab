330bf1877a44f8654c07242a1d336dea989eac66
unknown <adwoapokua.eommm.b@gmail.com>
2023-11-17 11:28:09 +0000

    jsupdate

---
+++ medi.js
@@ -0,0 +1,168 @@
+// const submitbtn = document.getElementById('submitbutton');
+
+
+// submitbtn.addEventListener('click',function(){
+//     alert('HEY BIRD');
+// });
+
+
+
+
+// const submitbtn = document.getElementById('submitbutton');
+
+// submitbtn.addEventListener('click', function() {
+//   alert('HEY BIRD');
+// });
+
+
+// JavaScript functions for hover effect
+var heartInfo = document.querySelector('.heartinfo');
+var icon = document.querySelector('.heartinfo i.fa');
+var textElements = document.querySelectorAll('.heartinfo .hearth3, .heartinfo .heartp');
+
+heartInfo.addEventListener('mouseover', function() {
+  // Change styles on hover
+  heartInfo.style.backgroundColor = '#3498db';
+  icon.style.backgroundColor = 'white';
+  icon.style.color = '#3498db';
+  textElements.forEach(function(element) {
+    element.style.color = 'white';
+  });
+});
+
+heartInfo.addEventListener('mouseout', function() {
+  // Reset styles on mouseout
+ heartInfo.style.backgroundColor = 'transparent';
+  icon.style.backgroundColor = 'rgb(25, 121, 204)';
+  icon.style.color = 'white';
+  textElements.forEach(function(element) {
+    element.style.color = 'rgb(17, 83, 141)';
+  });
+});
+
+
+var chairInfo = document.querySelector('.chairinfo');
+var icon = document.querySelector('.chairinfo i.fa');
+var textElements = document.querySelectorAll('.chairinfo .chairh3, .chairinfo .chairtp');
+
+chairInfo.addEventListener('mouseover', function() {
+  // Change styles on hover
+  chairInfo.style.backgroundColor = '#3498db';
+  icon.style.backgroundColor = 'white';
+  icon.style.color = '#3498db';
+  textElements.forEach(function(element) {
+    element.style.color = 'white';
+  });
+});
+
+chairInfo.addEventListener('mouseout', function() {
+  // Reset styles on mouseout
+ chairInfo.style.backgroundColor = 'transparent';
+  icon.style.backgroundColor = 'rgb(25, 121, 204)';
+  icon.style.color = 'white';
+  textElements.forEach(function(element) {
+    element.style.color = 'rgb(17, 83, 141)';
+  });
+});
+
+
+
+var blindInfo = document.querySelector('.blindinfo');
+var icon = document.querySelector('.blindinfo i.fa');
+var textElements = document.querySelectorAll('.blindinfo .blindh3, .blindinfo .blindp');
+
+blindInfo.addEventListener('mouseover', function() {
+  // Change styles on hover
+  blindInfo.style.backgroundColor = '#3498db';
+  icon.style.backgroundColor = 'white';
+  icon.style.color = '#3498db';
+  textElements.forEach(function(element) {
+    element.style.color = 'white';
+  });
+});
+
+blindInfo.addEventListener('mouseout', function() {
+  // Reset styles on mouseout
+ blindInfo.style.backgroundColor = 'transparent';
+  icon.style.backgroundColor = 'rgb(25, 121, 204)';
+  icon.style.color = 'white';
+  textElements.forEach(function(element) {
+    element.style.color = 'rgb(17, 83, 141)';
+  });
+});
+
+
+var deafInfo = document.querySelector('.deafinfo');
+var icon = document.querySelector('.deafinfo i.fa');
+var textElements = document.querySelectorAll('.deafinfo .deafh3, .deafinfo .deafp');
+
+deafInfo.addEventListener('mouseover', function() {
+  // Change styles on hover
+  deafInfo.style.backgroundColor = '#3498db';
+  icon.style.backgroundColor = 'white';
+  icon.style.color = '#3498db';
+  textElements.forEach(function(element) {
+    element.style.color = 'white';
+  });
+});
+
+deafInfo.addEventListener('mouseout', function() {
+  // Reset styles on mouseout
+ deafInfo.style.backgroundColor = 'transparent';
+  icon.style.backgroundColor = 'rgb(25, 121, 204)';
+  icon.style.color = 'white';
+  textElements.forEach(function(element) {
+    element.style.color = 'rgb(17, 83, 141)';
+  });
+});
+
+
+var eyeInfo = document.querySelector('.eyeinfo');
+var icon = document.querySelector('.eyeinfo i.fa');
+var textElements = document.querySelectorAll('.eyeinfo .eyeh3, .eyeinfo .eyep');
+
+eyeInfo.addEventListener('mouseover', function() {
+  // Change styles on hover
+  eyeInfo.style.backgroundColor = '#3498db';
+  icon.style.backgroundColor = 'white';
+  icon.style.color = '#3498db';
+  textElements.forEach(function(element) {
+    element.style.color = 'white';
+  });
+});
+
+eyeInfo.addEventListener('mouseout', function() {
+  // Reset styles on mouseout
+ eyeInfo.style.backgroundColor = 'transparent';
+  icon.style.backgroundColor = 'rgb(25, 121, 204)';
+  icon.style.color = 'white';
+  textElements.forEach(function(element) {
+    element.style.color = 'rgb(17, 83, 141)';
+  });
+});
+
+
+
+var plusInfo = document.querySelector('.plusinfo');
+var icon = document.querySelector('.plusinfo i.fa');
+var textElements = document.querySelectorAll('.plusinfo .plush3, .plusinfo .plusp');
+
+plusInfo.addEventListener('mouseover', function() {
+  // Change styles on hover
+  plusInfo.style.backgroundColor = '#3498db';
+  icon.style.backgroundColor = 'white';
+  icon.style.color = '#3498db';
+  textElements.forEach(function(element) {
+    element.style.color = 'white';
+  });
+});
+
+plusInfo.addEventListener('mouseout', function() {
+  // Reset styles on mouseout
+ plusInfo.style.backgroundColor = 'transparent';
+  icon.style.backgroundColor = 'rgb(25, 121, 204)';
+  icon.style.color = 'white';
+  textElements.forEach(function(element) {
+    element.style.color = 'rgb(17, 83, 141)';
+  });
+});
